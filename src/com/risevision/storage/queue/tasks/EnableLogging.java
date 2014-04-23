f27a8bf3127e96156959503df097a3a751f3e264
Alex Deaconu <alex.deaconu@risevision.com>
2014-04-23 12:07:13 -0400

    Checking Log Bucket ACLs and updating if needed

--- src/com/risevision/storage/queue/tasks/EnableLogging.java
+++ src/com/risevision/storage/queue/tasks/EnableLogging.java
@@ -10,6 +10,7 @@ import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpResponseException;
 import com.google.api.services.storage.Storage;
 import com.google.api.services.storage.model.Bucket;
+import com.google.api.services.storage.model.BucketAccessControl;
 import com.google.appengine.api.search.Document;
 import com.google.appengine.api.search.GetRequest;
 import com.google.appengine.api.search.GetResponse;
@@ -18,6 +19,7 @@ import com.google.appengine.api.search.IndexSpec;
 import com.google.appengine.api.search.SearchServiceFactory;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskOptions.Method;
+import com.risevision.common.client.utils.RiseUtils;
 import com.risevision.directory.documents.Company;
 import com.risevision.storage.Globals;
 import com.risevision.storage.MediaLibraryService;
@@ -35,51 +37,119 @@ public class EnableLogging extends AbstractTask {
 //			+ "</Logging>";
 //
 //	private static final String LOGGING_DISABLED_XML = "<Logging/>";
+	
+	private static final String PROJECTION_NOACL = "noacl";
+	private static final String PROJECTION_FULL = "full";
+	
+	private static final String STORAGE_ANALYTICS_EMAIL = "cloud-storage-analytics@google.com";
+	private static final String SCOPE_WRITE = "WRITER";
+	
+	private static Storage storage;
 
 	private static int QUERY_LIMIT = 200;
 	
 	public static String runJob(String userId, String lastCompanyId) {
 		
-//		try {
+		try {
 			
-			List<String> companyIds = getCompanyIDs(lastCompanyId);
+			storage = StorageHelper.getStorage(userId);
 			
-			if (companyIds.isEmpty()) {
-				return "Done";
-			}
+		} catch (IOException e) {
+			log.severe("Storage Initialization failed" + e.getMessage());
 			
-			for (String companyId: companyIds) {
-				boolean enabled = false;
-				try {
-					enabled = verifyBucketLogging(companyId, userId);
-
-					log.info(String.format("Company %s has logging %s", companyId, enabled ? "enabled" : "disabled"));
-
-				} catch (ServiceFailedException e) {
-					if (e.getReason() == ServiceFailedException.FORBIDDEN) {
-						return "forbidden";
-					}
-				}
-				
-				lastCompanyId = companyId;
-				
-			}
-			
-//			QueueFactory.getQueue(QueueName.STORAGE_LOG_ENABLE).add(withUrl("/queue")
-//					.param(QueryParam.TASK, QueueTask.RUN_ENABLE_LOGGING_JOB)
-//					.param(QueryParam.JOB_CURSOR, lastCompanyId)
-//					.countdownMillis(1000 * 5)
-//					.method(Method.GET));
-			
-			return "queued next job";
-			
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			
-//			throw new ServiceFailedException(ServiceFailedException.SERVER_ERROR);
-//		}
+			return "Error";
+		}
 		
+		// Job is starting, verify Logging bucket ACLs
+		if (RiseUtils.strIsNullOrEmpty(lastCompanyId)) {
+			try {
+				verifyLoggingBucketACL();
+			} catch (ServiceFailedException e) {
+				if (e.getReason() == ServiceFailedException.FORBIDDEN) {
+					return "forbidden";
+				}
+			}
+
+		}
+
+		List<String> companyIds = getCompanyIDs(lastCompanyId);
+		
+		if (companyIds.isEmpty()) {
+			return "Done";
+		}
+		
+		for (String companyId: companyIds) {
+			boolean enabled = false;
+			try {
+				enabled = verifyBucketLogging(companyId);
+
+				log.info(String.format("Company %s has logging %s", companyId, enabled ? "enabled" : "disabled"));
+
+			} catch (ServiceFailedException e) {
+				if (e.getReason() == ServiceFailedException.FORBIDDEN) {
+					return "forbidden";
+				}
+			}
+			
+			lastCompanyId = companyId;
+			
+		}
+		
+//		QueueFactory.getQueue(QueueName.STORAGE_LOG_ENABLE).add(withUrl("/queue")
+//				.param(QueryParam.TASK, QueueTask.RUN_ENABLE_LOGGING_JOB)
+//				.param(QueryParam.JOB_CURSOR, lastCompanyId)
+//				.countdownMillis(1000 * 5)
+//				.method(Method.GET));
+		
+		return "queued next job";
+			
+	}
+	
+	private static boolean verifyLoggingBucketACL() throws ServiceFailedException {
+		String bucketName = Globals.LOGS_BUCKET_NAME;
+		
+		try {
+		    Storage.Buckets.Get getBucket = storage.buckets().get(bucketName);
+		    getBucket.setProjection(PROJECTION_FULL);
+		    Bucket bucket = getBucket.execute();
+		    
+		    for (BucketAccessControl acl: bucket.getAcl()) {
+		    	
+		    	if (acl.getEmail() != null && acl.getEmail().equals(STORAGE_ANALYTICS_EMAIL)) {
+			    	log.info("Logs bucket configured correctly: " + bucketName);
+
+		    		return true;
+		    	}
+		
+		    }
+		    
+		    // ACL not found
+		    Storage.Buckets.Update updateBucket = storage.buckets().update(bucketName, bucket);
+		    
+		    BucketAccessControl acl = new BucketAccessControl();
+		    
+		    acl.setBucket(bucketName);
+		    acl.setEmail(STORAGE_ANALYTICS_EMAIL);
+		    acl.setEntity("group-" + STORAGE_ANALYTICS_EMAIL);
+		    acl.setRole(SCOPE_WRITE);
+		    
+		    bucket.getAcl().add(acl);
+		    
+		    updateBucket.execute();
+		    
+	    	log.info("Logs bucket ACL updated: " + bucketName);
+			    
+	    } catch (GoogleJsonResponseException e) {
+			if (e.getStatusCode() != ServiceFailedException.NOT_FOUND) {
+				log.warning(e.getStatusCode() + " - " + e.getMessage());
+			}
+	    	
+			throw new ServiceFailedException(e.getStatusCode());
+		} catch (IOException e) {
+	    	log.warning("An error occurred: " + e);
+	    }
+		
+		return false;
 	}
 	
 	private static List<String> getCompanyIDs(String lastId) {
@@ -105,30 +175,27 @@ public class EnableLogging extends AbstractTask {
 
 	} 
 	
-	public static boolean verifyBucketLogging(String companyId, String userId) throws ServiceFailedException {
+	public static boolean verifyBucketLogging(String companyId) throws ServiceFailedException {
 
 		String bucketName = MediaLibraryService.getBucketName(companyId);
 
-		Storage storage;
-
 		try {
-			storage = StorageHelper.getStorage(userId);
-		
+			
 		    Storage.Buckets.Get getBucket = storage.buckets().get(bucketName);
-		    getBucket.setProjection("noacl");
+		    getBucket.setProjection(PROJECTION_NOACL);
 		    Bucket bucket = getBucket.execute();
 		    
 		    if (bucket.getLogging() == null || !Globals.LOGS_BUCKET_NAME.equals(bucket.getLogging().getLogBucket())) {
 		    	
-//			    Storage.Buckets.Update updateBucket = storage.buckets().update(bucketName, bucket);
-//			    
-//			    Bucket.Logging logging = new Bucket.Logging();
-//			    logging.setLogBucket(Globals.LOGS_BUCKET_NAME);
-//			    logging.setLogObjectPrefix(bucketName);
-//			
-//			    bucket.setLogging(logging);
-//			    
-//			    updateBucket.execute();
+			    Storage.Buckets.Update updateBucket = storage.buckets().update(bucketName, bucket);
+			    
+			    Bucket.Logging logging = new Bucket.Logging();
+			    logging.setLogBucket(Globals.LOGS_BUCKET_NAME);
+			    logging.setLogObjectPrefix(bucketName);
+			
+			    bucket.setLogging(logging);
+			    
+			    updateBucket.execute();
 		
 				return false;
 		
