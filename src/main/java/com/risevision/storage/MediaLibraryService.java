d2c31921dc6468c7f8adff18e213ef5676defa44
tejohnso <mail@tyler-johnson.ca>
2014-06-14 23:19:25 -0400

    Clean up comment blocks and whitespace

--- src/main/java/com/risevision/storage/MediaLibraryService.java
+++ src/main/java/com/risevision/storage/MediaLibraryService.java
@@ -16,142 +16,129 @@ import com.risevision.storage.info.MediaItemInfo;
 import com.risevision.storage.info.ServiceFailedException;
 
 public abstract class MediaLibraryService {
-	private static MediaLibraryService instance; 
-	/** Global instance of the HTTP transport. */
-	protected static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-	
-	protected static final Logger log = Logger.getAnonymousLogger();
+  private static MediaLibraryService instance; 
+  protected static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
 
-	public static MediaLibraryService getInstance() {
-		try {
-			if (instance == null)
-				instance = new MediaLibraryServiceImpl();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return instance;
-	}
-	
-	public abstract ListAllMyBucketsResponse getAllMyBuckets() throws ServiceFailedException;
-	
-	public abstract InputStream getBucketProperty(String bucketName, String property) throws ServiceFailedException;
-	
-	public abstract String getBucketPropertyString(String bucketName, String property) throws ServiceFailedException;
-	
-	public List<MediaItemInfo> getBucketItems(String bucketName) throws ServiceFailedException {
-		return getBucketItems(bucketName, null, null);
-	}
-	
-	public abstract List<MediaItemInfo> getBucketItems(String bucketName, String prefix, String marker) throws ServiceFailedException;
-	
-	public String getBucketItemsString(String bucketName) throws ServiceFailedException {
-		return getBucketItemsString(bucketName, null, null);
-	}
-	
-	public String getBucketItemsString(String bucketName, String prefix, String marker) throws ServiceFailedException {
-		String response = "";
+  protected static final Logger log = Logger.getAnonymousLogger();
 
-		try {
-			JSONWriter stringer;
-			stringer = new JSONStringer();
-			
-//			getBucketProperty(bucketName, "logging");
-//			getBucketProperty(bucketName, "lifecycle");
+  public static MediaLibraryService getInstance() {
+    try {
+      if (instance == null)
+        instance = new MediaLibraryServiceImpl();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return instance;
+  }
 
-			List<MediaItemInfo> items = getBucketItems(bucketName, prefix, marker);
-			stringer.object();
-			stringer.key("status").value(ServiceFailedException.OK);
-			stringer.key("mediaFiles");
-			stringer.array();
-			for (MediaItemInfo item: items) {
-				stringer.object();
-				
-				stringer.key("key").value(item.getKey());
-				stringer.key("lastModified").value(item.getLastModified() != null ? item.getLastModified().getTime() : "");
-//				stringer.key("sizeString").value(item.getSizeString());
-				stringer.key("size").value(item.getSize());
-				stringer.key("eTag").value(item.geteTag());
-				
-				stringer.endObject();
-			}
-			stringer.endArray();
-			stringer.endObject();
+  public abstract ListAllMyBucketsResponse getAllMyBuckets() 
+  throws ServiceFailedException;
 
-//			ListAllMyBucketsResponse allbuckets = getAllMyBuckets();
-//			stringer.array();
-//			int count = 0; 
-//			if (allbuckets != null) {
-//				for (Object item: allbuckets.entries) {
-//					Bucket bucket = (Bucket) item;
-//					
-//					stringer.object();
-//
-//					count++;
-//					stringer.key("count").value(count);
-//					stringer.key("name").value(bucket.name);
-//					stringer.key("creationDate").value(bucket.creationDate);
-//					
-//					stringer.endObject();
-//				}
-//			}
-//			stringer.endArray();
-			
-			response = stringer.toString();		
+  public abstract InputStream getBucketProperty(String bucketName,
+                                                String property)
+  throws ServiceFailedException;
 
-//			response = getBucketPropertyString(bucketName, "logging");
-//
-//			response += "\n";
-//			response += Boolean.toString(MediaLibraryLogReader.verifyBucketLogging(bucketName));
-//			response += "\n";
-//			
-//			response += getBucketPropertyString(bucketName, "logging");
-			
+  public abstract String getBucketPropertyString(String bucketName,
+                                                 String property)
+  throws ServiceFailedException;
 
-		} catch (JSONException e) {
-			log.severe("Error - " + e.getMessage());
+  public List<MediaItemInfo> getBucketItems(String bucketName)
+  throws ServiceFailedException {
+    return getBucketItems(bucketName, null, null);
+  }
 
-			e.printStackTrace();
-		}
-		
-		return response;
-	}
-	
-	public static String getBucketName(String companyId) throws ServiceFailedException {
-		if (Strings.isNullOrEmpty(companyId)) {
-			throw new ServiceFailedException(ServiceFailedException.SERVER_ERROR);
-		}
-		
-		return "risemedialibrary-" + companyId;
-	}
-	
-	public abstract void createBucket(String bucketName) throws ServiceFailedException;
-	
-	public abstract void updateBucketProperty(String bucketName, String property, String propertyXMLdoc) throws ServiceFailedException;
-	
-	public abstract boolean deleteMediaItem(String bucketName, String itemName) throws ServiceFailedException;
-	
-	public List<String> deleteMediaItems(String bucketName, List<String> itemNames) {
-		List<String> failedItems = new ArrayList<>();
-		
-		for (String itemName : itemNames) {
-			try {
-				if (!deleteMediaItem(bucketName, itemName)) {
-					failedItems.add(itemName);
-				}
-			} catch (ServiceFailedException e) {
-				if (e.getReason() != ServiceFailedException.NOT_FOUND) {
-					failedItems.add(itemName);
-				}
-			}
-		}
-		
-		return failedItems;
-	}
-	
-	public abstract InputStream getMediaItem(String bucketName, String itemName) throws ServiceFailedException;
-	
-	public abstract String getMediaItemUrl(String bucketName, String itemName) throws Exception;
-	
-	public abstract String getSignedPolicy(String policyBase64);
+  public abstract List<MediaItemInfo> getBucketItems(String bucketName,
+                                                     String prefix,
+                                                     String marker)
+  throws ServiceFailedException;
 
+  public String getBucketItemsString(String bucketName)
+  throws ServiceFailedException {
+    return getBucketItemsString(bucketName, null, null);
+  }
+
+  public String getBucketItemsString(String bucketName,
+                                     String prefix,
+                                     String marker)
+  throws ServiceFailedException {
+    String response = "";
+
+    try {
+      JSONWriter stringer;
+      stringer = new JSONStringer();
+      List<MediaItemInfo> items = getBucketItems(bucketName, prefix, marker);
+      stringer.object();
+      stringer.key("status").value(ServiceFailedException.OK);
+      stringer.key("mediaFiles");
+      stringer.array();
+      for (MediaItemInfo item: items) {
+        stringer.object();
+
+        stringer.key("key").value(item.getKey());
+        stringer.key("lastModified").value(item.getLastModified() != null ?
+          item.getLastModified().getTime() : "");
+        stringer.key("size").value(item.getSize());
+        stringer.key("eTag").value(item.geteTag());
+
+        stringer.endObject();
+      }
+      stringer.endArray();
+      stringer.endObject();
+
+      response = stringer.toString();		
+    } catch (JSONException e) {
+      log.severe("Error - " + e.getMessage());
+      e.printStackTrace();
+    }
+
+    return response;
+  }
+
+  public static String getBucketName(String companyId)
+  throws ServiceFailedException {
+    if (Strings.isNullOrEmpty(companyId)) {
+      throw new ServiceFailedException(ServiceFailedException.SERVER_ERROR);
+    }
+
+    return "risemedialibrary-" + companyId;
+  }
+
+  public abstract void createBucket(String bucketName)
+  throws ServiceFailedException;
+
+  public abstract void updateBucketProperty(String bucketName,
+                                            String property,
+                                            String propertyXMLdoc)
+  throws ServiceFailedException;
+
+  public abstract boolean deleteMediaItem(String bucketName,
+                                          String itemName)
+  throws ServiceFailedException;
+
+  public List<String> deleteMediaItems(String bucketName,
+                                       List<String> itemNames) {
+    List<String> failedItems = new ArrayList<>();
+
+    for (String itemName : itemNames) {
+      try {
+        if (!deleteMediaItem(bucketName, itemName)) {
+          failedItems.add(itemName);
+        }
+      } catch (ServiceFailedException e) {
+        if (e.getReason() != ServiceFailedException.NOT_FOUND) {
+          failedItems.add(itemName);
+        }
+      }
+    }
+
+    return failedItems;
+  }
+
+  public abstract InputStream getMediaItem(String bucketName, String itemName)
+  throws ServiceFailedException;
+
+  public abstract String getMediaItemUrl(String bucketName, String itemName)
+  throws Exception;
+
+  public abstract String getSignedPolicy(String policyBase64);
 }
