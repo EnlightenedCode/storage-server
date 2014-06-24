08a883db94588330ba47af8e014d91cee85d1775
tejohnso <mail@tyler-johnson.ca>
2014-06-24 13:00:24 -0400

    Add devserver awareness to Globals

--- src/main/java/com/risevision/storage/ServletContextListener.java
+++ src/main/java/com/risevision/storage/ServletContextListener.java
@@ -2,6 +2,7 @@ package com.risevision.storage;
 
 import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
+import com.risevision.storage.Globals;
 
 public class ServletContextListener implements javax.servlet.ServletContextListener {
   private static final Logger log = Logger.getAnonymousLogger();
@@ -9,6 +10,11 @@ public class ServletContextListener implements javax.servlet.ServletContextListe
   @Override
   public void contextInitialized(ServletContextEvent event) {
     log.info("Initializing servlet.");
+    if (Globals.devserver) {
+      log.info("Running on local devserver");
+    } else {
+      log.info("Running on hosted GAE");
+    }
     log.info("Using the following logging globals:");
     log.info(Globals.LOGGING_ENABLED_XML);
     log.info(Globals.LOGS_BUCKET_NAME);
