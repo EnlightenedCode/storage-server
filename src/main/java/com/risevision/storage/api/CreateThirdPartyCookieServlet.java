933a6e723657043ffe584844c75a43a345abb3f8
Andrew Alanis <andrew4336@gmail.com>
2014-10-10 01:01:51 -0600

    Third Party Cookies Blocked bug fix
    added Servlet that Creates a Third Party test Cookie
    
    created servlet for Checking test cookie
    
    Test associated with CheckThirdPartyCookiesServlet
    
    Test created for CreateThirdPartyCookiesServlet
    
    updated web.xml for new servlet paths

---
+++ src/main/java/com/risevision/storage/api/CreateThirdPartyCookieServlet.java
@@ -0,0 +1,48 @@
+package com.risevision.storage.api;
+
+import javax.servlet.http.*;
+import java.io.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateThirdPartyCookieServlet extends HttpServlet {
+    private final Cookie cookie;
+    //List of allowed origins
+    private final List<String> incomingUrls;
+
+    public CreateThirdPartyCookieServlet(){
+        this.incomingUrls = Arrays.asList("http://storage.risevision.com","http://localhost:8000");
+        this.cookie = new Cookie("third_party_c_t","third_party_c_t");
+    }
+
+    public CreateThirdPartyCookieServlet(Cookie c, List<String> domains){
+        this.incomingUrls = domains;
+        this.cookie = c;
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        // Get client's origin
+        String clientOrigin = request.getHeader("origin");
+        int myIndex = incomingUrls.indexOf(clientOrigin);
+        if(myIndex != -1) {
+            response.addCookie(cookie);
+            response.setHeader("Access-Control-Allow-Origin", clientOrigin);
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+        }
+        PrintWriter out = response.getWriter();
+        String javascript = "{\"completed\": \"true\"}";
+        out.print(javascript);
+        out.flush();
+    }
+
+    public Cookie getCookie(){
+        return this.cookie;
+    }
+}
