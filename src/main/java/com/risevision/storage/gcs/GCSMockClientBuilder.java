6c6c760516e0a13efd57e8ba78e6646b5e20c0ca
tejohnso <mail@tyler-johnson.ca>
2014-10-17 13:15:08 -0400

    Add task tests

---
+++ src/main/java/com/risevision/storage/gcs/GCSMockClientBuilder.java
@@ -0,0 +1,60 @@
+package com.risevision.storage.gcs;
+
+import java.io.IOException;
+
+import com.google.api.client.googleapis.testing.json.*;
+import com.google.api.client.googleapis.testing.services.json.*;
+import com.google.api.client.json.*;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.testing.http.*;
+import com.google.api.client.http.*;
+
+import com.google.api.services.storage.*;
+import com.google.api.services.storage.model.*;
+
+public class GCSMockClientBuilder {
+  HttpTransport transport;
+  final JsonFactory jsonFactory = new JacksonFactory();
+
+  public GCSMockClientBuilder
+  (final String mockResponse, final int mockStatusCode) {
+    transport = new MockHttpTransport() {
+      public LowLevelHttpRequest buildRequest(String name, String url) {
+        return new MockLowLevelHttpRequest() {
+          public LowLevelHttpResponse execute() throws IOException {
+            MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+            result.setStatusCode(mockStatusCode);
+            result.setContentType(Json.MEDIA_TYPE);
+            result.setContent(mockResponse);
+            if (mockStatusCode > 299) {
+              String jsonErrorMessage = "Mocked server side error";
+              throw GoogleJsonResponseExceptionFactoryTesting.newMock
+              (jsonFactory, mockStatusCode, jsonErrorMessage);
+            }
+            return result;
+          }
+        };
+      }
+    };
+  }
+
+  public GCSMockClientBuilder() {
+    this("{}", 200);
+  }
+
+  public GCSMockClientBuilder(String response) {
+    this(response, 200);
+  }
+
+  public GCSMockClientBuilder(int statusCode) {
+    this("{}", statusCode);
+  }
+
+  public Storage build() {
+    return new Storage.Builder
+    (transport, jsonFactory, null)
+    .setApplicationName("Test Applicatoin").build();
+  }
+}
+
+
---
+++ src/test/java/servertasks/AddPublicReadObjectServerTaskTest.java
@@ -0,0 +1,55 @@
+package com.risevision.storage.servertasks;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.IOException;
+
+import com.risevision.storage.gcs.GCSMockClientBuilder;
+import com.google.api.services.storage.Storage;
+import com.google.api.services.storage.model.ObjectAccessControl;
+
+public class AddPublicReadObjectServerTaskTest {
+
+  Storage mockGCSClient = new GCSMockClientBuilder(200).build();
+  Map<String, String[]> requestParams = new HashMap<>();
+
+  @Test (expected = IllegalArgumentException.class)
+  public void itThrowsWhenNoBucketSpecified() {
+    requestParams.put("object", new String[] {"fileName"});
+    AddPublicReadObjectServerTask task = new AddPublicReadObjectServerTask
+    (mockGCSClient, requestParams);
+  }
+
+  @Test (expected = IllegalArgumentException.class)
+  public void itThrowsWhenNoObjectSpecified() {
+    requestParams.put("bucket", new String[] {"bucketName"});
+    AddPublicReadObjectServerTask task = new AddPublicReadObjectServerTask
+    (mockGCSClient, requestParams);
+  }
+
+  @Test (expected = IOException.class)
+  public void itThrowsOnServerError() throws IOException {
+    requestParams.put("object", new String[] {"fileName"});
+    requestParams.put("bucket", new String[] {"bucketName"});
+
+    AddPublicReadObjectServerTask task = new AddPublicReadObjectServerTask
+    (new GCSMockClientBuilder(404).build(), requestParams);
+
+    task.handleRequest();
+  }
+
+  @Test public void itReturnsOnSuccess() throws IOException {
+    requestParams.put("object", new String[] {"fileName"});
+    requestParams.put("bucket", new String[] {"bucketName"});
+
+    AddPublicReadObjectServerTask task = new AddPublicReadObjectServerTask
+    (mockGCSClient, requestParams);
+
+    task.handleRequest();
+  }
+}
+
