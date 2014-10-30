c4cc79dc0965cfab5632c4d20ee7a954fb21ca6f
Andrew Alanis <andrew4336@gmail.com>
2014-10-30 14:57:15 -0600

    added more descriptive comments
    and changed some better variable names that make more sense.

--- src/main/java/com/risevision/storage/datastore/datastoreService.java
+++ src/main/java/com/risevision/storage/datastore/datastoreService.java
@@ -30,9 +30,12 @@ public class datastoreService {
 
     public void addShareFolder(String companyId, String sharedCompanyId, String folder, boolean view, boolean edit, boolean add) throws ServiceFailedException {
         SharePermission permToAdd = new SharePermission(sharedCompanyId,view,edit,add);
+        //check to see if there is a folderName already created under that originCompanyId
         ShareFolder shrCheck = ofy().load().type(ShareFolder.class).filter("originCompanyId", companyId).filter("folderName", folder).first().now();
         if(shrCheck != null) {
+            //if there is an entry, check to see if the permission entry is identical...this compares sharedCompanyId to see if the same shared company is already present.
             if(shrCheck.permissions.contains(permToAdd)){
+                // if it is present update / edit the permissions
                 SharePermission tmp = shrCheck.permissions.get(shrCheck.permissions.indexOf(permToAdd));
                 tmp.view = view;
                 tmp.edit = edit;
@@ -40,13 +43,15 @@ public class datastoreService {
                 shrCheck.permissions.set(shrCheck.permissions.indexOf(permToAdd), tmp);
                 ofy().save().entity(shrCheck).now();
             } else {
+                //if not present add a new SharePermission object
                 shrCheck.permissions.add(permToAdd);
                 ofy().save().entity(shrCheck).now();
             }
         } else {
+            //if entry for Sharefolder is not there at all add it with a new permission object inside it's permissions list.
             List<SharePermission> accessList = new ArrayList<SharePermission>();
-            ShareFolder shrFold = new ShareFolder(companyId, folder, accessList);
             accessList.add(permToAdd);
+            ShareFolder shrFold = new ShareFolder(companyId, folder, accessList);
             ofy().save().entity(shrFold).now();
             assert shrFold.id != null;
         }
@@ -54,12 +59,15 @@ public class datastoreService {
 
     public void addShareFolderLink(String companyId, String sharedCompanyId, String folder, boolean view, boolean edit, boolean add) throws ServiceFailedException{
         ShareFolderLink shared = new ShareFolderLink(companyId, sharedCompanyId, folder, view, edit, add);
-        ShareFolderLink fetched2 = ofy().load().type(ShareFolderLink.class).filter("originCompanyId", companyId).filter("folderName", folder).first().now();
-        if(fetched2 == null) {
+        //check if the sharedLink is already there with teh same originCompanyId and folderName.
+        ShareFolderLink sharedLinkCheck = ofy().load().type(ShareFolderLink.class).filter("originCompanyId", companyId).filter("folderName", folder).first().now();
+        if(sharedLinkCheck == null) {
+            // if not there, then add one.
             ofy().save().entity(shared).now();
             assert shared.id != null;
         } else {
-            ofy().delete().entity(fetched2).now();
+            //if it is there, delete the old entry and add the new entry. this handles updating / editing the permissions as well.
+            ofy().delete().entity(sharedLinkCheck).now();
             ofy().save().entity(shared).now();
             assert shared.id != null;
         }
