4d7025cd328898811ce249f3023b26337f6dd245
Andrew Alanis <andrew4336@gmail.com>
2014-10-30 14:01:11 -0600

    added this to GCSFilesResponse to get json array
    this could be put in a seperate response file too if needed so it won't be mixed in the the files response.

--- src/main/java/com/risevision/storage/api/responses/GCSFilesResponse.java
+++ src/main/java/com/risevision/storage/api/responses/GCSFilesResponse.java
@@ -4,16 +4,20 @@ import java.util.List;
 
 import com.google.api.services.storage.model.StorageObject;
 import com.google.appengine.api.users.User;
+import com.risevision.storage.entities.ShareFolder;
+import org.json.JSONArray;
 
 public class GCSFilesResponse extends SimpleResponse {
 	
 	public List<StorageObject> files;
+    public List<ShareFolder> sharedFolders;
 	
 	public GCSFilesResponse(User user){
 		
                   super(user);
 		
 		this.files = null;
+        this.sharedFolders = null;
 	}
 
 }
