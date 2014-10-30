3744bda75d3ebb7c416f13c098b1def72c71ceff
Andrew Alanis <andrew4336@gmail.com>
2014-10-30 14:07:36 -0600

    made ShareFolderLink class
    I needed a different data structure and class to insert and keep track of share folder links that is more flat and does not group by folder name.
    This way when the client wants to get the folders that are shared from other companies...it can just filter this list by sharedCompanyId and get the folderNames. I added permissions just incase I need it.

---
+++ src/main/java/com/risevision/storage/entities/ShareFolderLink.java
@@ -0,0 +1,36 @@
+package com.risevision.storage.entities;
+
+import com.googlecode.objectify.annotation.Cache;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+import java.io.Serializable;
+
+/**
+ * Created by Andrew on 10/29/2014.
+ */
+@Entity
+@Cache
+public class ShareFolderLink implements Serializable{
+    @Id public Long id;
+    @Index public String originCompanyId;
+    @Index public String sharedCompanyId;
+    public String folderName;
+    public boolean view;
+    public boolean edit;
+    public boolean add;
+
+    public ShareFolderLink() {
+
+    }
+
+    public ShareFolderLink (String originCompanyId, String sharedCompanyId, String folderName, boolean view, boolean edit, boolean add){
+        this.originCompanyId = originCompanyId;
+        this.sharedCompanyId = sharedCompanyId;
+        this.folderName = folderName;
+        this.view = view;
+        this.edit = edit;
+        this.add = add;
+    }
+}
