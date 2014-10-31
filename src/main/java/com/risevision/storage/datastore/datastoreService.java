ec8b2d03816cca314302607a8f44e9130b3a51f0
Andrew Alanis <andrew4336@gmail.com>
2014-10-31 16:54:13 -0600

    added removeShare and updated getShared
    cleaned up tabs and use spaces instead in my IDE.
    Added removeShareFolderLink that deletes given a companyId, folderName, sharedCompanyId combination.
    Refactored getSharedFolders to accept new nullables and query off them.

--- src/main/java/com/risevision/storage/datastore/datastoreService.java
+++ src/main/java/com/risevision/storage/datastore/datastoreService.java
@@ -1,5 +1,6 @@
 package com.risevision.storage.datastore;
 
+import com.googlecode.objectify.cmd.Query;
 import com.risevision.storage.entities.ShareFolderLink;
 import com.risevision.storage.info.ServiceFailedException;
 
@@ -12,37 +13,52 @@ import static com.risevision.storage.datastore.OfyService.ofy;
  */
 public class datastoreService {
 
-    private static datastoreService instance;
-    private datastoreService() {}
+  private static datastoreService instance;
+  private datastoreService() {}
 
-    public static datastoreService getInstance() {
-        try {
-            if (instance == null) {
-                instance = new datastoreService();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return instance;
+  public static datastoreService getInstance() {
+    try {
+      if (instance == null) {
+          instance = new datastoreService();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return instance;
+  }
 
-    public void addShareFolderLink(String companyId, String sharedCompanyId, String folder, boolean view, boolean edit, boolean add) throws ServiceFailedException{
-        ShareFolderLink shared = new ShareFolderLink(companyId, sharedCompanyId, folder, view, edit, add);
-        //check if the sharedLink is already there with teh same originCompanyId and folderName.
-        ShareFolderLink sharedLinkCheck = ofy().load().type(ShareFolderLink.class).filter("originCompanyId", companyId).filter("folderName", folder).first().now();
-        if(sharedLinkCheck == null) {
-            // if not there, then add one.
-            ofy().save().entity(shared).now();
-            assert shared.id != null;
-        } else {
-            //if it is there, delete the old entry and add the new entry. this handles updating / editing the permissions as well.
-            ofy().delete().entity(sharedLinkCheck).now();
-            ofy().save().entity(shared).now();
-            assert shared.id != null;
-        }
+  public void addShareFolderLink(String companyId, String sharedCompanyId, String folder, boolean view, boolean edit, boolean add) throws ServiceFailedException{
+    ShareFolderLink shared = new ShareFolderLink(companyId, sharedCompanyId, folder, view, edit, add);
+    //check if the sharedLink is already there with the same originCompanyId and folderName.
+    ShareFolderLink sharedLinkCheck = ofy().load().type(ShareFolderLink.class).filter("originCompanyId", companyId).filter("folderName", folder).first().now();
+    if(sharedLinkCheck == null) {
+      // if not there, then add one.
+      ofy().save().entity(shared).now();
+      assert shared.id != null;
+    } else {
+      //if it is there, delete the old entry and add the new entry. this handles updating / editing the permissions as well.
+      ofy().delete().entity(sharedLinkCheck).now();
+      ofy().save().entity(shared).now();
+      assert shared.id != null;
     }
+  }
 
-    public List<ShareFolderLink> getSharedFolders(String companyId) throws ServiceFailedException {
-        return ofy().load().type(ShareFolderLink.class).filter("originCompanyId", companyId).list();
+  public void removeShareFolderLink(String companyId, String sharedCompanyId, String folder) throws ServiceFailedException{
+    //check if the sharedLink is already there with thr same originCompanyId and folderName.
+    ShareFolderLink sharedLinkCheck = ofy().load().type(ShareFolderLink.class).filter("originCompanyId", companyId)
+      .filter("folderName", folder)
+      .filter("sharedCompanyId", sharedCompanyId)
+      .first().now();
+    if(sharedLinkCheck != null) {
+      //if it is there, delete the old entry and add the new entry. this handles updating / editing the permissions as well.
+      ofy().delete().entity(sharedLinkCheck).now();
     }
+  }
+
+  public List<ShareFolderLink> getSharedFolders(String companyId, String sharedCompanyId, String folder) throws ServiceFailedException {
+    Query<ShareFolderLink> query = ofy().load().type(ShareFolderLink.class).filter("originCompanyId", companyId);
+    query = sharedCompanyId != null ? query.filter("sharedCompanyId", sharedCompanyId) : query;
+    query = folder != null ? query.filter("folderName", folder) : query;
+    return query.list();
+  }
 }
