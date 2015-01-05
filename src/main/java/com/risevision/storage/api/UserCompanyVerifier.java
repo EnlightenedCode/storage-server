b0964473d87acaa7564e61202ff2ec8930f78b94
tejohnso <mail@tyler-johnson.ca>
2015-01-04 21:20:48 -0500

    Create public ServiceAccountAPIRequestor
    To be used for any google api enpoint with service account
    authorization

--- src/main/java/com/risevision/storage/api/UserCompanyVerifier.java
+++ src/main/java/com/risevision/storage/api/UserCompanyVerifier.java
@@ -4,41 +4,17 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.logging.Logger;
 
-import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpResponse;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.risevision.storage.Globals;
-import com.risevision.storage.gcs.LocalCredentialBuilder;
+import com.risevision.storage.gcs.ServiceAccountAPIRequestor;
 import com.risevision.storage.info.ServiceFailedException;
 
 public class UserCompanyVerifier {
   private static final String HTTP_CHARSET = "UTF-8";
   private static final Logger log = Logger.getAnonymousLogger();
-  private static GoogleCredential credential;
-  private static HttpRequestFactory httprequestFactory;
-
-  public UserCompanyVerifier() {
-      if (credential == null){
-          credential = new LocalCredentialBuilder().getCredentialFromP12File
-                  (Globals.RVCORE_P12_PATH, Globals.RVCORE_ID, Globals.EMAIL_SCOPE);
-      }
-      if(httprequestFactory == null){
-          httprequestFactory = new UrlFetchTransport().createRequestFactory();
-      }
-  }
-
-  private HttpResponse makeRequest(GenericUrl url, HttpRequest request) throws IOException {
-
-      if (credential.getExpirationTimeMilliseconds() < 60000) {credential.refreshToken();}
-      request.setInterceptor(credential);
-
-      return request.execute();
-  }
 
   private String replaceWithLocalUserEmail(String email) throws ServiceFailedException {
       if (Globals.devserver) {
@@ -56,7 +32,6 @@ public class UserCompanyVerifier {
 
   public void verifyUserCompany(String companyId, String email)
   throws ServiceFailedException {
-
     email = replaceWithLocalUserEmail(email);
 
     log.info("Verifying company access for user " + email);
@@ -69,8 +44,8 @@ public class UserCompanyVerifier {
       .replace("COMPANYID", URLEncoder.encode(companyId, HTTP_CHARSET))
       .replace("EMAIL", URLEncoder.encode(email, HTTP_CHARSET)));
 
-      HttpRequest request = httprequestFactory.buildPostRequest(url, null);
-      HttpResponse response = makeRequest(url,request);
+      HttpResponse response = ServiceAccountAPIRequestor.makeRequest
+      (ServiceAccountAPIRequestor.SERVICE_ACCOUNT.CORE, "POST", url, null);
 
       if (!response.parseAsString().contains("\"allowedAccess\": true")) {
         throw new ServiceFailedException(ServiceFailedException.FORBIDDEN);
