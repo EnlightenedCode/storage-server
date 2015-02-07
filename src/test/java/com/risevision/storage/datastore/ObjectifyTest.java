f75a4f8a06389e6cae8ed5121ce47153f792aa38
Francisco Vallarino <fjvallarino@gmail.com>
2015-02-07 01:46:38 -0300

    Remove unused imports

--- src/test/java/com/risevision/storage/datastore/ObjectifyTest.java
+++ src/test/java/com/risevision/storage/datastore/ObjectifyTest.java
@@ -1,15 +1,13 @@
 package com.risevision.storage.datastore;
 
-import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
-import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import com.googlecode.objectify.annotation.Entity;
-import com.googlecode.objectify.annotation.Id;
-import com.googlecode.objectify.annotation.Index;
+import java.io.Closeable;
+import java.io.IOException;
+
 import org.junit.After;
 import org.junit.Before;
 
-import java.io.Closeable;
-import java.io.IOException;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 /**
  * Created by rodrigopavezi on 12/10/14.
